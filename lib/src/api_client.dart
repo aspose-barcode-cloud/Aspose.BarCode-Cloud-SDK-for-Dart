5c765bef71d88155268e62964a4fb8d986757c5c
TimurBaiguskarovAspose <134321067+TimurBaiguskarovAspose@users.noreply.github.com>
2026-03-19 15:26:05 +0500

    Release 26.3 (#64)
    * Release 26.3

--- lib/src/api_client.dart
+++ lib/src/api_client.dart
@@ -10,7 +10,7 @@ import 'api_helper.dart';
 import 'auth/authentication.dart';
 
 /// Current SDK Version
-const SDK_VERSION = "4.26.2";
+const SDK_VERSION = "4.26.3";
 
 /// ApiClient is responsible for making HTTP requests to the API.
 class ApiClient {
